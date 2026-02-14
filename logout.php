613e2cb45387c3b2073e1a3f9fd69ab327d99f2c
farhanxi6000 <frhnxi6000@gmail.com>
2026-02-14 21:41:46 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,13 +1,10 @@
 <?php
 session_start();
 
-/* Clear all session variables */
 $_SESSION = [];
 
-/* Destroy the session */
 session_destroy();
 
-/* Redirect back to landing page */
 header("Location: index.php");
 exit;
 ?>

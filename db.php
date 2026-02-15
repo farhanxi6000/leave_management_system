75f619b110af5169d43800d7afe497123c51b551
farhanxi6000 <frhnxi6000@gmail.com>
2026-02-15 13:11:15 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "leave_management_system";
+$host = "sql302.infinityfree.com";
+$user = "if0_41060566";
+$pass = "Jaqujim123";
+$db   = "if0_41060566_leave_management_system";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
